7bb364ccec866702c02ccbe4c117a80a007037e9
Fahad Hasan <44fahadhasan@gmail.com>
2024-10-20 11:27:44 +0600

    react animated cursor added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AnimatedCursor from "react-animated-cursor";
 import { RouterProvider } from "react-router-dom";
 import ThemeProvider from "../context/ThemeProvider";
 import routes from "../routes/Routes";
@@ -6,6 +7,14 @@ const App = () => {
   return (
     <>
       <ThemeProvider>
+        <AnimatedCursor
+          color="99, 102, 241"
+          innerSize={8}
+          outerSize={35}
+          innerScale={1}
+          outerScale={1.7}
+          outerAlpha={0.2}
+        />
         <RouterProvider router={routes} />
       </ThemeProvider>
     </>
